00ffb87416e49396b0d63bce29d76b7686812c9c
Arifur999 <arr9677@gmail.com>
2025-03-02 21:31:27 +0600

    completed assingnmet 5

--- js/main.js
+++ js/main.js
@@ -1,13 +1,101 @@
+let comButtons = document.querySelectorAll(".com-button");
+for (let i = 0; i < comButtons.length; i++) {
+    const comBtn = comButtons[i];
+
+    comBtn.addEventListener("click", function () {
+        comBtn.style.pointerEvents = "none";
+        event.target.classList.add("bg-gray-300");
+
+        const num = document.getElementById("num").innerText;
+        const sumNum = document.getElementById("sum-num").innerText;
+        const convertedNum = parseInt(num);
+        const convertedSumNum = parseInt(sumNum);
+
+        if (convertedSumNum > 0) {
+            const sum = convertedSumNum + 1;
+            document.getElementById("sum-num").innerText = sum;
+
+        }
+        if (convertedNum > 0) {
+            const subtraction = convertedNum - 1;
+            document.getElementById("num").innerText = subtraction;
+            alert("Bord updated sucssesful");
+        }
+        if (convertedNum <= 1) {
+            alert("congrates!!!you complete all task");
+        }
+
+
+
+
+        let taskHead = document.querySelectorAll(".task-head")
+
+   
+
+
+                let now = new Date();
+                let formattedTime = now.toLocaleDateString("en-US", {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: true
+
+                })
+
+
+
+                document.getElementById("activitys")
+                let allTask = document.getElementById("all-task")
+                let newParagraph = document.createElement('p');
+                newParagraph.classList.add(
+                    'p-4',
+                    'bg-[#F4F7FF]',
+                    'rounded-lg',
+                    'm-2'
+
+                );
+                newParagraph.innerText = ` ${taskHead}at ${formattedTime}`
+                allTask.appendChild(newParagraph);
+
+          
+
+        
+
+
+    })
+
+}
+document.getElementById("clear-history").addEventListener("click",function(){
+    document.getElementById("all-task").innerHTML="";
+});
+
+
+document.getElementById("theme-btn").addEventListener("click",function(){
+
+})
+function getRandomColor(){
+    let getRandomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    return getRandomColor;
+}
+document.getElementById("theme-btn").addEventListener("click",function(){
+    document.body.style.backgroundColor=getRandomColor();
+})
+
+
+document.getElementById("home-dsk").addEventListener("click",function(){
+    window.location.href = "second.html";
+
+})
 
 
 
 function dateTime() {
     let now = new Date();
-    let option ={weekday: 'short',month:'short',day:'numeric',year:'numeric'};
-    let toDate = now.toLocaleDateString('en-US',option);
+    let option = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+    let toDate = now.toLocaleDateString('en-US', option);
 
     document.getElementById("new-date").innerText = toDate;
-    
+
 }
 dateTime();
 
